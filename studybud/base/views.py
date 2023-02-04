f4ca6794ec18f7b12b5ce4ae222dc4b96ca7ca42
yazan <yazanzeidany95@gmail.com>
2023-02-05 01:22:58 +0300

    add rooms urls with pk

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id':1, 'name': 'lets learn python!'},
+    {'id':2, 'name': 'Desgin with me'},
+    {'id':3, 'name': 'frontend developers'}
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(reuqest):
-    return render(reuqest, 'room.html')
+def room(reuqest, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}        
+
+    return render(reuqest, 'base/room.html', context)
