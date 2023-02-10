673be60646e284ff14c58d9f1847a613cf8afd4f
yazan <yazanzeidany95@gmail.com>
2023-02-10 17:01:48 +0300

    adding new models and forms

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Room, Topic, Message
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Topic)
+admin.site.register(Message)
+
+
